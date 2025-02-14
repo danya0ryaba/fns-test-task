61af58cbc9eb17a65d8aa046f195c195f3b1b0fb
Daniil Ryabenkov <danilryaba0@gmail.com>
2025-02-15 00:54:33 +0300

    init: init repo

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Error } from './pages/Error'
+import { Home } from './pages/Home'
+import { Create } from './pages/Create'
+
+function App() {
+
+  return <>
+    <Routes>
+
+      <Route path='/' element={<Home />} />
+
+      <Route path='create' element={<Create />} />
+
+      <Route path='*' element={<Error />} />
+
+    </Routes>
+  </>
+}
+
+export default App
